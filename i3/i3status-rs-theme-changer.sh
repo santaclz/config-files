5dc3423342f67abf0ea34b6af097e0dd345f891a
santaclz <rabit0110115@gmail.com>
2021-04-13 23:29:09 +0200

    Update i3status-rs-theme-changer.sh

--- i3/i3status-rs-theme-changer.sh
+++ i3/i3status-rs-theme-changer.sh
@@ -5,7 +5,7 @@ source ~/.cache/wal/colors.sh
 echo "idle_bg = \"$background\"
 idle_fg = \"$foreground\"
 
-info_bg = \"$color3\"
+info_bg = \"$color2\"
 info_fg = \"$background\"
 
 good_bg = \"$color4\"
@@ -14,7 +14,7 @@ good_fg = \"$background\"
 warning_bg = \"$color4\"
 warning_fg = \"$background\"
 
-critical_bg = \"$color4\"
+critical_bg = \"$color1\"
 critical_fg = \"$background\"
 
 separator = \"\"
@@ -22,4 +22,4 @@ separator_bg = \"auto\"
 separator_fg = \"auto\"
 
 alternating_tint_bg = \"#000000\"
-alternating_tint_fg = \"#000000\"" > mytheme.toml
+alternating_tint_fg = \"#000000\"" > ~/.config/i3/mytheme.toml
